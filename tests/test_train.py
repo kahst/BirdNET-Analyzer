2405baa29ba5a35ee3edb897771c9a4b5de313a0
Bengt Lüers <Bengt.Lueers@gmail.com>
2023-06-11 18:08:24 +0000

    Use a named tuple instead of a dictionary

--- tests/test_train.py
+++ tests/test_train.py
@@ -2,29 +2,32 @@
 
 Can be used to train a custom classifier with new training data.
 """
-
+from collections import namedtuple
 
 import config as cfg
 from birdnet.train.train_model import train_model
 
 
 def test_train():
-    args = {
-        'i': 'train_data/',
-        'o': 'checkpoints/custom/Custom_Classifier.tflite',
-        'epochs': 100,
-        'batch_size': 32,
-        'learning_rate': 0.01,
-        'hidden_units': 0,
-    }
+    Arguments = namedtuple(
+        'Arguments', "i o epochs batch_size learning_rate hidden_units"
+    )
+    arguments = Arguments(
+        i='train_data/',
+        o='checkpoints/custom/Custom_Classifier.tflite',
+        epochs=100,
+        batch_size=32,
+        learning_rate=0.01,
+        hidden_units=0,
+    )
 
     # Config
-    cfg.TRAIN_DATA_PATH = args.i
-    cfg.CUSTOM_CLASSIFIER = args.o
-    cfg.TRAIN_EPOCHS = args.epochs
-    cfg.TRAIN_BATCH_SIZE = args.batch_size
-    cfg.TRAIN_LEARNING_RATE = args.learning_rate
-    cfg.TRAIN_HIDDEN_UNITS = args.hidden_units
+    cfg.TRAIN_DATA_PATH = arguments.i
+    cfg.CUSTOM_CLASSIFIER = arguments.o
+    cfg.TRAIN_EPOCHS = arguments.epochs
+    cfg.TRAIN_BATCH_SIZE = arguments.batch_size
+    cfg.TRAIN_LEARNING_RATE = arguments.learning_rate
+    cfg.TRAIN_HIDDEN_UNITS = arguments.hidden_units
 
     # Train model
     train_model()
