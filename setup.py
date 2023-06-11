d8d43c9399cc805508518e9a66ed576f3f41926f
Bengt Lüers <Bengt.Lueers@gmail.com>
2023-06-11 22:27:12 +0000

    Add an initial setup,py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='birdnet',
+    version='2.4.0',
+    url='https://github.com/kahst/BirdNET-Analyzer',
+    author='Stefan Kahl',
+    author_email='sk2487@cornell.edu',
+    description='BirdNET analyzer for scientific audio data processing.',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'tensorflow',
+        'bottle',
+        'resampy',
+        'librosa',
+    ],
+)
