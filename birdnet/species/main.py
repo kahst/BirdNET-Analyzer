838747b3492ea26f3c0c67550fa59e1724742f03
Bengt Lüers <bengt.lueers@gmail.com>
2023-06-12 02:51:32 +0200

    Fix imports

--- birdnet/species/main.py
+++ birdnet/species/main.py
@@ -7,34 +7,8 @@ import os
 import sys
 
 from birdnet.configuration import config
-from birdnet.model import main
-from birdnet.utils.line_reading import read_lines
-from birdnet.model.main import explore
-
-
-def get_species_list(lat: float, lon: float, week: int, threshold=0.05, sort=False) -> List[str]:
-    """Predict a species list.
-
-    Uses the model to predict the species list for the given coordinates and filters by threshold.
-
-    Args:
-        lat: The latitude.
-        lon: The longitude.
-        week: The week of the year [1-48]. Use -1 for year-round.
-        threshold: Only values above or equal to threshold will be shown.
-        sort: If the species list should be sorted.
-
-    Returns:
-        A list of all eligible species.
-    """
-    # Extract species from model
-    pred = explore(lat, lon, week)
-
-    # Make species list
-    slist = [p[1] for p in pred if p[0] >= threshold]
-
-    return sorted(slist) if sort else slist
-
+from birdnet.species.species_list_getting import get_species_list
+from birdnet.utils.lines_reading import read_lines
 
 if __name__ == "__main__":
     # Parse arguments
