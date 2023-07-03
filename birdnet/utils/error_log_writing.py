071b9d6d2655790b545a2aec404c9c7a08f16153
Bengt Lüers <bengt.lueers@gmail.com>
2023-07-03 18:26:16 +0200

    Reformat

--- birdnet/utils/error_log_writing.py
+++ birdnet/utils/error_log_writing.py
@@ -5,7 +5,9 @@ from birdnet.configuration import config
 
 def write_error_log(ex: Exception):
     """Writes an exception to the error log.
-    Formats the stacktrace and writes it in the error log file configured in the config.
+    Formats the stacktrace and writes it in the error log file configured in
+    the config.
+
     Args:
         ex: An exception that occurred.
     """
