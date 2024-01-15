9e23983b18ccfbcc75416e066ad889efe0563456
Stefan Kahl <kahst@hrz.tu-chemnitz.de>
2024-01-15 13:32:58 +0100

    species range model update

--- checkpoints/V2.4/BirdNET_GLOBAL_6K_V2.4_Model_TFJS/app.py
+++ checkpoints/V2.4/BirdNET_GLOBAL_6K_V2.4_Model_TFJS/app.py
@@ -7,4 +7,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
--- config.py
+++ config.py
@@ -16,7 +16,7 @@ MODEL_VERSION: str = 'V2.4'
 PB_MODEL: str = 'checkpoints/V2.4/BirdNET_GLOBAL_6K_V2.4_Model'
 # MODEL_PATH = PB_MODEL # This will load the protobuf model
 MODEL_PATH: str = 'checkpoints/V2.4/BirdNET_GLOBAL_6K_V2.4_Model_FP32.tflite'
-MDATA_MODEL_PATH: str = 'checkpoints/V2.4/BirdNET_GLOBAL_6K_V2.4_MData_Model_FP16.tflite'
+MDATA_MODEL_PATH: str = 'checkpoints/V2.4/BirdNET_GLOBAL_6K_V2.4_MData_Model_V2_FP16.tflite'
 LABELS_FILE: str = 'checkpoints/V2.4/BirdNET_GLOBAL_6K_V2.4_Labels.txt'
 TRANSLATED_LABELS_PATH:str = 'labels/V2.4'
 
