e2c638f2978c47622b8e3b9d1f8995ecb127e7c4
Josef Haupt <josef.haupt@informatik.tu-chemnitz.de>
2025-01-20 15:11:46 +0100

    .

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ author = 'Stefan Kahl'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["myst_parser"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -23,5 +23,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
