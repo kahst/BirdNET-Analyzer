9271acba32bd8c067957adca3231dfdad60504a8
Josef Haupt <josef.haupt@informatik.tu-chemnitz.de>
2025-01-20 15:53:00 +0100

    autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -6,22 +6,26 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'BirdNET-Analyzer'
-copyright = '2025, Stefan Kahl'
-author = 'Stefan Kahl'
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+
+project = "BirdNET-Analyzer"
+copyright = "2025, Stefan Kahl"
+author = "Stefan Kahl"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
+extensions = ["myst_parser", "sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
