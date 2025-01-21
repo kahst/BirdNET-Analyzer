e747231af8c9ad62c9a4e1f1dc65c23211ab58d4
Josef Haupt <josef.haupt@informatik.tu-chemnitz.de>
2025-01-21 15:16:21 +0100

    .

--- docs/conf.py
+++ docs/conf.py
@@ -26,6 +26,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.autosummary",
 ]
+autosummary_generate = True
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
--- docs/index.rst
+++ docs/index.rst
@@ -14,5 +14,6 @@ documentation for details.
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
+   
    api
 
