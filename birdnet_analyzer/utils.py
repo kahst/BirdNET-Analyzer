e3698f695fc8afcfa5b754367c841142bda3d2f6
Josef Haupt <josef.haupt@informatik.tu-chemnitz.de>
2025-02-07 11:33:27 +0100

    ai generated util docs

--- birdnet_analyzer/utils.py
+++ birdnet_analyzer/utils.py
@@ -636,6 +636,14 @@ def save_result_file(result_path: str, out_string: str):
 
 
 def io_args():
+    """
+    Creates an argument parser for input and output paths.
+    Returns:
+        argparse.ArgumentParser: The argument parser with input and output path arguments.
+    Arguments:
+        input (str): Path to the input file or folder. Defaults to the value specified in cfg.INPUT_PATH.
+        output (str): Path to the output folder. Defaults to the input path if not specified.
+    """
     p = argparse.ArgumentParser(add_help=False)
     p.add_argument(
         "input",
@@ -650,6 +658,15 @@ def io_args():
 
 
 def bandpass_args():
+    """
+    Creates an argument parser for bandpass filter frequency arguments.
+    This function sets up an argument parser with two arguments:
+    --fmin and --fmax, which define the minimum and maximum frequencies
+    for the bandpass filter, respectively. The values are constrained
+    to be within the range defined by cfg.SIG_FMIN and cfg.SIG_FMAX.
+    Returns:
+        argparse.ArgumentParser: The configured argument parser.
+    """
     p = argparse.ArgumentParser(add_help=False)
     p.add_argument(
         "--fmin",
@@ -668,6 +685,19 @@ def bandpass_args():
 
 
 def species_args():
+    """
+    Creates an argument parser for species-related arguments.
+    Returns:
+        argparse.ArgumentParser: The argument parser with the following arguments:
+            --lat (float): Recording location latitude. Set -1 to ignore. Default is -1.
+            --lon (float): Recording location longitude. Set -1 to ignore. Default is -1.
+            --week (int): Week of the year when the recording was made. Values in [1, 48] (4 weeks per month). 
+                          Set -1 for year-round species list. Default is -1.
+            --slist (str): Path to species list file or folder. If folder is provided, species list needs to be named 
+                           "species_list.txt". If lat and lon are provided, this list will be ignored.
+            --sf_thresh (float): Minimum species occurrence frequency threshold for location filter. Values in [0.01, 0.99]. 
+                                 Defaults to cfg.LOCATION_FILTER_THRESHOLD.
+    """
     p = argparse.ArgumentParser(add_help=False)
     p.add_argument("--lat", type=float, default=-1, help="Recording location latitude. Set -1 to ignore.")
     p.add_argument("--lon", type=float, default=-1, help="Recording location longitude. Set -1 to ignore.")
@@ -692,6 +722,15 @@ def species_args():
 
 
 def sigmoid_args():
+    """
+    Creates an argument parser for sigmoid sensitivity.
+    This function sets up an argument parser with a single argument `--sensitivity`.
+    The sensitivity value is constrained to be within the range [0.5, 1.5], where higher
+    values result in higher detection sensitivity. The default value is taken from
+    `cfg.SIGMOID_SENSITIVITY`.
+    Returns:
+        argparse.ArgumentParser: The argument parser with the sensitivity argument configured.
+    """
     p = argparse.ArgumentParser(add_help=False)
     p.add_argument(
         "--sensitivity",
@@ -704,6 +743,14 @@ def sigmoid_args():
 
 
 def overlap_args(help_string=f"Overlap of prediction segments. Values in [0.0, 2.9]. Defaults to {cfg.SIG_OVERLAP}."):
+    """
+    Creates an argument parser for the overlap of prediction segments.
+    Args:
+        help_string (str): A custom help string for the overlap argument. Defaults to a formatted string 
+                           indicating the range [0.0, 2.9] and the default value from cfg.SIG_OVERLAP.
+    Returns:
+        argparse.ArgumentParser: An argument parser with the overlap argument configured.
+    """
     p = argparse.ArgumentParser(add_help=False)
     p.add_argument(
         "--overlap",
@@ -716,6 +763,16 @@ def overlap_args(help_string=f"Overlap of prediction segments. Values in [0.0, 2
 
 
 def audio_speed_args():
+    """
+    Creates an argument parser for audio speed configuration.
+    This function sets up an argument parser with a single argument `--audio_speed`
+    which allows the user to specify a speed factor for audio playback. The speed factor
+    must be a float value where values less than 1.0 will slow down the audio and values
+    greater than 1.0 will speed it up. The minimum allowed value is 0.01. The default
+    value is taken from the configuration (`cfg.AUDIO_SPEED`).
+    Returns:
+        argparse.ArgumentParser: The argument parser with the `--audio_speed` argument configured.
+    """
     p = argparse.ArgumentParser(add_help=False)
     p.add_argument(
         "--audio_speed",
@@ -728,6 +785,14 @@ def audio_speed_args():
 
 
 def threads_args():
+    """
+    Creates an argument parser for specifying the number of CPU threads to use.
+    The parser adds an argument `--threads` (or `-t`) which accepts an integer value.
+    The value is constrained to be at least 1. If not specified, the default value is
+    set to half the number of available CPU cores, but not exceeding 8.
+    Returns:
+        argparse.ArgumentParser: The argument parser with the `--threads` argument.
+    """
     import multiprocessing
 
     p = argparse.ArgumentParser(add_help=False)
@@ -743,6 +808,18 @@ def threads_args():
 
 
 def min_conf_args():
+    """
+    Creates an argument parser for the minimum confidence threshold.
+
+    Returns:
+        argparse.ArgumentParser: An argument parser with the --min_conf argument.
+
+    The --min_conf argument:
+        - Sets the minimum confidence threshold for predictions.
+        - Accepts float values in the range [0.01, 0.99].
+        - Defaults to the value specified in cfg.MIN_CONFIDENCE.
+        - Ensures that the provided value is clamped between 0.01 and 0.99.
+    """
     p = argparse.ArgumentParser(add_help=False)
     p.add_argument(
         "--min_conf",
@@ -753,6 +830,15 @@ def min_conf_args():
 
 
 def locale_args():
+    """
+    Creates an argument parser for locale settings.
+    This function creates an argument parser with a single argument `--locale` 
+    (or `-l`) which specifies the locale for translated species common names. 
+    The default value is 'en' (US English). The available locale values include 
+    'af', 'en_UK', 'de', 'it', and others.
+    Returns:
+        argparse.ArgumentParser: An argument parser with the locale argument.
+    """
     p = argparse.ArgumentParser(add_help=False)
     p.add_argument(
         "-l",
@@ -765,6 +851,14 @@ def locale_args():
 
 
 def bs_args():
+    """
+    Creates an argument parser for batch size configuration.
+    Returns:
+        argparse.ArgumentParser: An argument parser with a batch size argument.
+    The parser includes the following argument:
+        -b, --batchsize: An integer specifying the number of samples to process at the same time.
+                         The value must be at least 1. Defaults to the value of cfg.BATCH_SIZE.
+    """
     p = argparse.ArgumentParser(add_help=False)
     p.add_argument(
         "-b",
