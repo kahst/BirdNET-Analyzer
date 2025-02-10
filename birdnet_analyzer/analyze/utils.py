a7d1d30d114c3a22cb758ca94c526f21c6aa7a6a
Josef Haupt <josef.haupt@informatik.tu-chemnitz.de>
2025-02-10 14:17:48 +0100

    .

--- birdnet_analyzer/analyze/utils.py
+++ birdnet_analyzer/analyze/utils.py
@@ -9,8 +9,8 @@ import numpy as np
 
 import birdnet_analyzer.audio as audio
 import birdnet_analyzer.config as cfg
-import birdnet_analyzer.utils as utils
 import birdnet_analyzer.model as model
+import birdnet_analyzer.utils as utils
 
 #                    0       1      2           3             4              5               6                7           8             9           10         11
 RAVEN_TABLE_HEADER = "Selection\tView\tChannel\tBegin Time (s)\tEnd Time (s)\tLow Freq (Hz)\tHigh Freq (Hz)\tCommon Name\tSpecies Code\tConfidence\tBegin Path\tFile Offset (s)\n"
