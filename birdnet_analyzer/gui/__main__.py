8d1236109fc35b54ebe03fc3df149d97029e274b
Josef Haupt <josef.haupt@informatik.tu-chemnitz.de>
2025-02-11 11:28:33 +0100

    added scripts to pyproject.toml (#566)

--- birdnet_analyzer/gui/__main__.py
+++ birdnet_analyzer/gui/__main__.py
@@ -1,18 +1,3 @@
-import birdnet_analyzer.gui.multi_file as mfa
-import birdnet_analyzer.gui.review as review
-import birdnet_analyzer.gui.segments as gs
-import birdnet_analyzer.gui.single_file as sfa
-import birdnet_analyzer.gui.species as species
-import birdnet_analyzer.gui.train as train
-import birdnet_analyzer.gui.utils as gu
+from birdnet_analyzer.gui import main
 
-gu.open_window(
-    [
-        sfa.build_single_analysis_tab,
-        mfa.build_multi_analysis_tab,
-        train.build_train_tab,
-        gs.build_segments_tab,
-        review.build_review_tab,
-        species.build_species_tab,
-    ]
-)
+main()
