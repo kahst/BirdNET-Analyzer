e2207f5ce4daf1f94cfe19661c6eb84a4d518bd3
Josef Haupt <josef.haupt@informatik.tu-chemnitz.de>
2025-02-14 16:33:10 +0100

    Add GitHub link to documentation (#585)

--- docs/conf.py
+++ docs/conf.py
@@ -37,9 +37,12 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+rst_prolog = ":github_url: https://github.com/kahst/BirdNET-Analyzer\n"
 html_theme = "sphinx_rtd_theme"
 html_favicon = "_static/birdnet-icon.ico"
 html_logo = "_static/birdnet_logo.png"
 html_static_path = ["_static"]
 html_css_files = ["css/custom.css"]
-html_theme_options = {"style_external_links": True, "github_url": "https://github.com/kahst/BirdNET-Analyzer"}
+html_theme_options = {"style_external_links": True}
+html_show_sourcelink = False
+html_show_sphinx = False
